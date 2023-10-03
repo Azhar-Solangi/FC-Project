1355842e4e95fdbb34426168c07bc1101f6a898b
Azhar-Solangi <azharsolangi.xwave@gmail.com>
2023-10-03 03:41:49 -0700

    3 days progress

---
+++ src/pages/HomePage/homepage.jsx
@@ -0,0 +1,11 @@
+import HeroSection from "./components/heroSection/heroSection";
+import Navbar from "./components/navbar/navbar";
+
+const HomePage =()=> {
+   return <>
+    <Navbar/>
+    <HeroSection/>
+    </>
+    
+} 
+export default HomePage;
